47bb82cd56ac7a1c6ffda6bd9e53650908fa5f3c
Davide Bellettini <davide.bellettini@gmail.com>
2013-02-09 12:10:23 +0100

    Simple service done

---
+++ common/appcommon.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'   => 'pdo_sqlite',
+        'path'     => __DIR__.'/../db/app.db',
+    ),
+));
--- web/app.php
+++ web/app.php
@@ -1,3 +1,27 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../common/appcommon.php';
+
+$app->get('/barcode/{barcode}', function($barcode) use ($app) {
+    $sql = 'SELECT uuid FROM barcodes WHERE barcode = ?';
+    $uuid = $app['db']->fetchColumn($sql, array($barcode));
+    
+    if(false === $uuid) {
+        $app['db']
+            ->executeUpdate('INSERT INTO barcodes (uuid, barcode) VALUES (?,?)', array(
+                $uuid = trim(`uuidgen -r`),
+                $barcode
+            ))
+        ;
+    }
+
+    return $app
+                ->json(array(
+                    'uuid' => $uuid,
+                    'barcode' => $barcode
+                ))
+           ;
+})->assert('barcode', '[0-9]{13,18}');
+
+$app['debug'] = true;
+$app->run();
