d7160c2173696d4c04f657776e851d8f86ae9e24
Davide Bellettini <davide.bellettini@gmail.com>
2013-11-25 08:21:15 +0100

    Console implementation close #1

---
+++ src/console.php
@@ -0,0 +1,24 @@
+<?php
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+
+$console = new Application('BarcodeBucket', '0.1.x-dev');
+$console->getDefinition()->addOption(new InputOption('--env', '-e', InputOption::VALUE_REQUIRED, 'The Environment name.', 'dev'));
+$console
+    ->register('database:init')
+    ->setDescription('Database initialization')
+    ->setCode(function (InputInterface $input, OutputInterface $output) use ($app) {
+        $app['db']->executeUpdate(<<<EOT
+CREATE TABLE barcodes (
+    uuid CHAR(36) PRIMARY KEY,
+    barcode CHAR(14) UNIQUE
+)
+EOT
+        );
+    })
+;
+
+return $console;
